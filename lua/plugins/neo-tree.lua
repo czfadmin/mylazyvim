ee11b72cfbc5d605fd6c40ed1667e216d51ffece
chenzhifu <2849007069@qq.com>
2024-04-29 06:05:54 +0000

    chore(neo-tree): 修改neotree配置

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,9 +1,59 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    git = {
-      ignore = false,
-      enable = true,
+    close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
+    enable_normal_mode_for_inputs = false, -- Enable normal mode for input dialogs.
+    open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+    sort_case_insensitive = false, -- used when sorting files and directories in the tree
+    sort_function = nil, -- use a custom function for sorting files and directories in the tree
+    filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = true, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          --"node_modules"
+        },
+        hide_by_pattern = { -- uses glob style patterns
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          ".DS_Store",
+          "thumbs.db",
+        },
+        never_show_by_pattern = { -- uses glob style patterns
+          --".null-ls_*",
+        },
+      },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+    },
+    window = {
+      ["a"] = {
+        "add",
+        -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+        -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+        config = {
+          show_path = "relative", -- "none", "relative", "absolute"
+        },
+      },
+      ["c"] = {
+        "copy",
+        config = {
+          show_path = "none", -- "none", "relative", "absolute"
+        },
+      },
     },
   },
   keys = {
