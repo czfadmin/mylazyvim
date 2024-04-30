70134e24e70b6111500b6ec8e1b66b9444bb3ab0
czfadmin <abchen9527@outlook.com>
2024-04-30 16:13:22 +0800

    feat(plugins): 懒加载部分插件

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,7 @@
 return {
-    "williamboman/mason.nvim",
-    opts = {
-        ensure_installed = {"stylua", "shellcheck", "shfmt", "flake8", "prettier"}
-    }
+  "williamboman/mason.nvim",
+  lazy = true,
+  opts = {
+    ensure_installed = { "stylua", "shellcheck", "shfmt", "flake8", "prettier" },
+  },
 }
